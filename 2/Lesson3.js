b668adaa56ca2fd9c202e8e9430a2c708e3af5b8
Ibrahim Awad <ibrahimawad98@hotmail.com>
2025-10-19 15:19:37 +0200

    Refactor sayHello and showUserAndPass functions

--- 2/Lesson3.js
+++ 2/Lesson3.js
@@ -1,11 +1,12 @@
-function sayHello() {  //دالة 
+function sayHello() {
     var name = window.txtName.value;
     document.getElementById("lbl").innerHTML = "Hello " + name;
 }
 
 function showUserAndPass() {
-    var user = window.txtUser.value; //قيمة متغير 
+    var user = window.txtUser.value; 
     var pass = window.txtPass.value; 
-    var msg = "<hr>" + user + "<hr>" + pass + "<hr>"; //متغير يلي بدي اعرضو
+    var msg = "<hr>" + user + "<hr>" + pass + "<hr>"; 
     window.lblmsg.innerHTML = msg;
+
 }
